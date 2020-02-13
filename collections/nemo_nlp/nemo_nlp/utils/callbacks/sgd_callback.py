6be27f634f8faffbf650e34ccb6635276504d4b8
Evelina Bakhturina <ebakhturina@nvidia.com>
2020-02-12 17:17:38 -0800

    joint goal acc updated
    Signed-off-by: Evelina Bakhturina <ebakhturina@nvidia.com>

--- collections/nemo_nlp/nemo_nlp/utils/callbacks/sgd_callback.py
+++ collections/nemo_nlp/nemo_nlp/utils/callbacks/sgd_callback.py
@@ -183,13 +183,8 @@ def eval_iter_callback(tensors, global_vars):
     cat_slot_status_labels = categorical_slot_status[cat_slot_status_mask]
     cat_slot_values_labels = categorical_slot_values[cat_slot_status_mask]
 
-    # active_cat_slots_mask = cat_slot_status_labels == data_utils.STATUS_ACTIVE
-    # cat_slot_status_correctness = cat_slot_status_labels == cat_slot_status_preds
-    # cat_slot_correctness = (cat_slot_values_labels == cat_slot_values_preds).type(torch.int)
-    # # slots are predicted correctly if the both the slot value and the slot status are correct
-    # cat_slot_correctness = cat_slot_correctness * cat_slot_status_correctness.type(torch.int)
-    # import pdb; pdb.set_trace()
-    # cat_batch_ids = get_batch_ids(cat_slot_status_mask)
+    # slots are predicted correctly if the both the slot value and the slot status are correct
+
 
 
     # determine if the slot status was predicted correctly
@@ -198,18 +193,15 @@ def eval_iter_callback(tensors, global_vars):
     # evaluate cat slot prediction only if slot is active
     # if predicted slot status = 0,2 (off/doncare) but the true status is 1 (active) => categorical correctness
     # for such example is 0
-    active_cat_slot_status_correctness = cat_slot_status_correctness * (cat_slot_status_labels == data_utils.STATUS_ACTIVE)
+    active_cat_slots = cat_slot_status_labels == data_utils.STATUS_ACTIVE
+    active_cat_slot_status_correctness = cat_slot_status_correctness * (active_cat_slots)
     cat_slot_values_correctness = (cat_slot_values_labels == cat_slot_values_preds).type(torch.int)
 
     cat_slot_correctness = torch.where(
         active_cat_slot_status_correctness, cat_slot_values_correctness, cat_slot_status_correctness.type(torch.int)
     )
-    # cat_slot_correctness = tensor2list(cat_slot_correctness)
-    # global_vars['cat_slot_correctness'].extend(tensor2list(cat_slot_correctness))
-    # global_vars['active_cat_slots'].extend(tensor2list(active_cat_slots_mask))
-
-    # global_vars['joint_cat_accuracy'].extend(get_cat_joint_accuracy(cat_slot_status_mask, cat_slot_correctness))
-
+    
+    cat_batch_ids = get_batch_ids(cat_slot_status_mask)
 
     # Noncategorical slots
     max_num_noncat_slots = noncategorical_slot_status.size()[-1]
@@ -227,14 +219,15 @@ def eval_iter_callback(tensors, global_vars):
     # slots with correctly predicted status
     noncat_slot_status_correctness = noncat_slot_status_labels == noncat_slot_status_preds
     # slots with correctly predicted ACTIVE status
-    active_noncat_slot_status_correctness = noncat_slot_status_correctness * (
-        noncat_slot_status_labels == data_utils.STATUS_ACTIVE
-    )
-
+    active_noncat_slots = noncat_slot_status_labels == data_utils.STATUS_ACTIVE
+    active_noncat_slot_status_correctness = noncat_slot_status_correctness * (active_noncat_slots)
+    
 
     ########################################
     # remove:
-    active_noncat_slot_status_correctness = noncat_slot_status_labels == data_utils.STATUS_ACTIVE
+    # noncat_slot_status_preds = noncat_slot_status_labels
+    # noncat_slot_value_start_preds = noncat_slot_value_start_labels
+    # noncat_slot_value_end_preds = noncat_slot_value_end_labels
     system_utterances = None
     ########################################
 
@@ -244,7 +237,7 @@ def eval_iter_callback(tensors, global_vars):
     for i in range(len(noncat_slot_status_labels)):
         if noncat_slot_status_correctness[i]:
             if noncat_slot_status_labels[i] == data_utils.STATUS_ACTIVE:
-                score = get_noncat_slot_value_match_score(
+                score = 5 * get_noncat_slot_value_match_score(
                                     i,
                                     noncat_slot_value_start_labels,
                                     noncat_slot_value_end_labels,
@@ -261,81 +254,62 @@ def eval_iter_callback(tensors, global_vars):
             score = 0
         noncat_slot_correctness.append(score)
 
-    print (noncat_slot_correctness)
-    import pdb; pdb.set_trace()
-
-    
-
-
-    # calculate number of correct predictions
-    nonactive_noncat_slot_status_correctness = noncat_slot_status_correctness * (noncat_slot_status_labels != data_utils.STATUS_ACTIVE)
-
-
-    # find indices of noncat slots for which predicted status was correctly predicted and is ACTIVE
-    inds_with_correct_active_noncat_slot_status = active_noncat_slot_status_correctness.type(torch.int).nonzero()
-
-    # use active_noncat_slot_status_correctness to select only slots with correctly predicted status ACTIVE
-    noncat_slot_value_start_labels = noncat_slot_value_start_labels[active_noncat_slot_status_correctness]
-    noncat_slot_value_end_labels = noncat_slot_value_end_labels[active_noncat_slot_status_correctness]
-    noncat_slot_value_start_preds = noncat_slot_value_start_preds[active_noncat_slot_status_correctness]
-    noncat_slot_value_end_preds = noncat_slot_value_end_preds[active_noncat_slot_status_correctness]
-
-    # get joint accuracies
-    
-
-    
-
-    # slot_values_preds = get_slot_values(
-    #     noncat_slot_value_start_preds,
-    #     noncat_slot_value_end_preds,
-    #     start_char_idxs,
-    #     end_char_idxs,
-    #     noncat_batch_ids[active_noncat_slot_status_correctness],
-    #     user_utterances,
-    #     system_utterances,
-    # )
-
-    # fuzzy_scores = get_fuzzy_scores(slot_values_true, slot_values_preds)
-    # fuzzy_scores = get_fuzzy_scores(slot_values_true, slot_values_true)
-
-    # import pdb; pdb.set_trace()
-
-    # print(fuzzy_scores)
-    # print()
-
-    # get joint goal accuracies
+    # combine cat and non cat slots for the frame
+    slots_correctness, is_active_slot, is_cat_slot = combine_cat_noncat_slots(cat_slot_correctness,
+                             noncat_slot_correctness,
+                             cat_batch_ids,
+                             noncat_batch_ids,
+                             active_cat_slots,
+                             active_noncat_slots)
 
 
-# def get_slot_values(
-#     noncat_slot_value_start,
-#     noncat_slot_value_end,
-#     start_char_idxs,
-#     end_char_idxs,
-#     batch_ids,
-#     user_utterances,
-#     system_utterances,
-# ):
+    for list_acc, slot_active, slot_cat in zip(slots_correctness, is_active_slot, is_cat_slot):
+        acc = get_average_and_joint_goal_accuracy(list_acc, slot_active, slot_cat)
+        print (acc)
 
-#     slot_values = []
-#     for i in range(len(noncat_slot_value_start)):
-#         tok_start_idx = noncat_slot_value_start[i]
-#         tok_end_idx = noncat_slot_value_end[i]
-#         ch_start_idx = start_char_idxs[batch_ids[i]][tok_start_idx]
-#         ch_end_idx = end_char_idxs[batch_ids[i]][tok_end_idx]
+def combine_cat_noncat_slots(cat_slot_correctness,
+                             noncat_slot_correctness,
+                             cat_batch_ids,
+                             noncat_batch_ids,
+                             active_cat_slots,
+                             active_noncat_slots):
 
-#         if ch_start_idx < 0 and ch_end_idx < 0:
-#             # Add span from the system utterance
-#             print('system utterance required')
-#             slot_values.append(None)
-#             # slot_values[slot] = (
-#             #     system_utterance[-ch_start_idx - 1:-ch_end_idx])
-#         elif ch_start_idx > 0 and ch_end_idx > 0:
-#             # Add span from the user utterance
-#             slot_values.append(user_utterances[batch_ids[i]][ch_start_idx - 1 : ch_end_idx])
-#         else:
-#             slot_values.append(None)
-#     return slot_values
+    cat_batch = collections.Counter(cat_batch_ids)
+    noncat_batch = collections.Counter(noncat_batch_ids)
 
+    batch_ids = sorted(cat_batch.keys() | noncat_batch.keys())
+
+    slot_correctness = []
+    is_active_slot = []
+    is_cat_slot = []
+    cat_start_ind = 0
+    noncat_start_ind = 0
+
+    for id in batch_ids:
+        frame_slots_correctness = []
+        is_active = []
+        is_cat = []
+        if id in cat_batch:
+            num_cat_slots_in_frame = cat_batch[id]
+            cat_end_ind = cat_start_ind + num_cat_slots_in_frame
+            frame_slots_correctness.extend(cat_slot_correctness[cat_start_ind:cat_end_ind])
+            
+            is_active.extend(active_cat_slots[cat_start_ind:cat_end_ind])
+            is_cat.extend([True] * num_cat_slots_in_frame)
+            cat_start_ind = cat_end_ind
+        if id in noncat_batch:
+            num_noncat_slots_in_frame = noncat_batch[id]
+            noncat_end_ind = noncat_start_ind + num_noncat_slots_in_frame
+            frame_slots_correctness.extend(noncat_slot_correctness[noncat_start_ind:noncat_end_ind])
+            
+            is_active.extend(active_noncat_slots[noncat_start_ind:noncat_end_ind])
+            is_cat.extend([False] * num_noncat_slots_in_frame)
+            noncat_start_ind = noncat_end_ind
+        slot_correctness.append(frame_slots_correctness)
+        is_active_slot.append(is_active)
+        is_cat_slot.append(is_cat)
+
+    return slot_correctness, is_active_slot, is_cat_slot
 
 
 def get_noncat_slot_value_match_score(
@@ -360,7 +334,7 @@ def get_noncat_slot_value_match_score(
         user_utterances,
         system_utterances)
 
-    str_value_pred = _get_noncat_slot_value(
+    str_pred = _get_noncat_slot_value(
         slot_idx,
         noncat_slot_value_start_preds,
         noncat_slot_value_end_preds,
@@ -371,14 +345,14 @@ def get_noncat_slot_value_match_score(
         system_utterances)
 
     if str_true is None:
-        if str_preds is None:
+        if str_pred is None:
             # if the slot value was mentioned in the previous utterances of the dialogue
             # that are not part of the current turn
             score = 1  # true and prediction don't modify previously set slot value
         else:
             score = 0  # preds incorrectly modifyes previously set slot value
     else:
-        score = fuzz.token_sort_ratio(str_true, str_preds) / 100.0
+        score = fuzz.token_sort_ratio(str_true, str_pred) / 100.0
 
     return score
 
@@ -412,20 +386,6 @@ def _get_noncat_slot_value(
     print (slot_value)
     return slot_value
 
-    # noncat_slot_correctness = get_noncat_slot_value_match(user_utterances,
-    #                                                       inds_with_correct_active_noncat_slot_status,
-    #                                                       noncat_slot_value_start_labels,
-    #                                                       noncat_slot_value_end_labels,
-    #                                                       noncat_slot_value_start_preds,
-    #                                                       noncat_slot_value_end_preds,
-    #                                                       num_noncategorical_slots[0])
-
-    # global_vars['noncat_slot_correctness'].extend(noncat_slot_correctness)
-    # import pdb; pdb.set_trace()
-    # print()
-    # joint_noncat_accuracy = torch.prod(noncat_slot_correctness.view(-1,num_noncategorical_slots[0]), -1)
-    # global_vars['joint_noncat_accuracy'].extend(tensor2list(joint_noncat_accuracy))
-
 def get_batch_ids(slot_status_mask):
     # determine batch_id slot active slot is associated with
     # it's needed to get the corresponing user utterance correctly
@@ -447,73 +407,6 @@ def get_joint_accuracy(slot_status_mask, slot_correctness_list):
     return joint_accuracy
 
 
-# def get_fuzzy_scores(slot_values_true_list, slot_values_preds_list):
-#     """Returns fuzzy string similarity score in range [0.0, 1.0]."""
-
-#     # The higher the score, the higher the similarity between the two strings
-#     fuzzy_scores = []
-
-#     for str_true, str_preds in zip(slot_values_true_list, slot_values_preds_list):
-#         if str_true is None:
-#             if str_preds is None:
-#                 # if the slot value was mentioned in the previous utterances of the dialogue
-#                 # that are not part of the current turn
-#                 score = 1  # true and prediction don't modify previously set slot value
-#             else:
-#                 score = 0  # preds incorrectly modifyes previously set slot value
-#         else:
-#             score = fuzz.token_sort_ratio(str_true, str_preds) / 100.0
-#         fuzzy_scores.append(score)
-#     return fuzzy_scores
-
-# def get_noncat_slot_value_match_score(true_slot_values_truet, slot_values_preds_list):
-#     """Returns fuzzy string similarity score in range [0.0, 1.0]."""
-
-#     # The higher the score, the higher the similarity between the two strings
-
-
-    
-#     if str_true is None:
-#         if str_preds is None:
-#             # if the slot value was mentioned in the previous utterances of the dialogue
-#             # that are not part of the current turn
-#             score = 1  # true and prediction don't modify previously set slot value
-#         else:
-#             score = 0  # preds incorrectly modifyes previously set slot value
-#     else:
-#         score = fuzz.token_sort_ratio(str_true, str_preds) / 100.0
-
-#     return score
-
-
-
-# def get_noncat_slot_value_match(
-#     user_utterances,
-#     indices,
-#     noncat_slot_value_start_labels,
-#     noncat_slot_value_end_labels,
-#     noncat_slot_value_start_preds,
-#     noncat_slot_value_end_preds,
-#     num_noncategorical_slots,
-# ):
-#     """Calculate non-categorical slots correctness.
-
-#     Args:
-#       str_ref_list: a list of reference strings.
-#       str_hyp: the hypothesis string.
-
-#     Returns:
-#     score: The highest fuzzy string match score of the references and hypotheis.
-#     """
-#     noncat_slot_correctness = []
-#     # user_utterance_ind = indices /
-#     for i, ind in enumerate(indices):
-#         user_utterance = user_utterances[indices / num_noncategorical_slots]
-#         str_label = user_utterance[noncat_slot_value_start_labels[ind] : noncat_slot_value_end_labels[ind]]
-#         str_preds = user_utterance[noncat_slot_value_start_preds[ind] : noncat_slot_value_end_preds[ind]]
-#         noncat_slot_correctness.append(max(0, fuzzy_string_match(str_label, str_preds)))
-
-#     return noncat_slot_correctness
 
 
 def eval_epochs_done_callback(global_vars):
@@ -526,7 +419,7 @@ def eval_epochs_done_callback(global_vars):
     requested_slot_status = np.asarray(global_vars['requested_slot_status'], dtype=int)
     req_slot_metrics = compute_f1(req_slot_predictions, requested_slot_status)
 
-    correctness_cat_slots = np.asarray(global_vars['cat_slot_correctness'], dtype=int)
+    # correctness_cat_slots = np.asarray(global_vars['cat_slot_correctness'], dtype=int)
     # joint_acc, turn_acc = \
     #     evaluate_metrics(global_vars['comp_res'],
     #                      global_vars['gating_labels'],
@@ -536,37 +429,37 @@ def eval_epochs_done_callback(global_vars):
     # gating_comp_flatten = (np.asarray(global_vars['gating_labels']) == np.asarray(global_vars['gating_preds'])).ravel()
     # gating_acc = np.sum(gating_comp_flatten) / len(gating_comp_flatten)
 
-    cat_slot_correctness = np.asarray(global_vars['cat_slot_correctness'])
+    # cat_slot_correctness = np.asarray(global_vars['cat_slot_correctness'])
     # noncat_slot_correctness = np.asarray(global_vars['noncat_slot_correctness'])
 
-    average_cat_accuracy = np.mean(cat_slot_correctness)
-    joint_cat_accuracy = np.mean(np.asarray(global_vars['joint_cat_accuracy'], dtype=int))
+    # average_cat_accuracy = np.mean(cat_slot_correctness)
+    # joint_cat_accuracy = np.mean(np.asarray(global_vars['joint_cat_accuracy'], dtype=int))
 
-    # average_noncat_accuracy = np.mean(noncat_slot_correctness)
-    # average_goal_accuracy = np.mean(np.concatenate((cat_slot_correctness, noncat_slot_correctness)))
+    # # average_noncat_accuracy = np.mean(noncat_slot_correctness)
+    # # average_goal_accuracy = np.mean(np.concatenate((cat_slot_correctness, noncat_slot_correctness)))
 
-    metrics = {
-        'all_services': {
-            # Active intent accuracy
-            "active_intent_accuracy": active_intent_accuracy,
-            "average_cat_accuracy": average_cat_accuracy,
-            # "average_goal_accuracy": average_goal_accuracy,
-            # "average_noncat_accuracy": average_noncat_accuracy,
-            "joint_cat_accuracy": joint_cat_accuracy,
-            # "joint_goal_accuracy": 0.4904726693494299,
-            # "joint_noncat_accuracy": 0.6226867035546613,
-            # Slot tagging F1
-            "requested_slots_f1": req_slot_metrics.f1,
-            "requested_slots_precision": req_slot_metrics.precision,
-            "requested_slots_recall": req_slot_metrics.recall,
-            # Average goal accuracy
-        }
-    }
+    # metrics = {
+    #     'all_services': {
+    #         # Active intent accuracy
+    #         "active_intent_accuracy": active_intent_accuracy,
+    #         "average_cat_accuracy": average_cat_accuracy,
+    #         # "average_goal_accuracy": average_goal_accuracy,
+    #         # "average_noncat_accuracy": average_noncat_accuracy,
+    #         "joint_cat_accuracy": joint_cat_accuracy,
+    #         # "joint_goal_accuracy": 0.4904726693494299,
+    #         # "joint_noncat_accuracy": 0.6226867035546613,
+    #         # Slot tagging F1
+    #         "requested_slots_f1": req_slot_metrics.f1,
+    #         "requested_slots_precision": req_slot_metrics.precision,
+    #         "requested_slots_recall": req_slot_metrics.recall,
+    #         # Average goal accuracy
+    #     }
+    # }
 
-    print('\n' + '#' * 50)
-    for k, v in metrics['all_services'].items():
-        print(f'{k}: {v}')
-    print('#' * 50 + '\n')
+    # print('\n' + '#' * 50)
+    # for k, v in metrics['all_services'].items():
+    #     print(f'{k}: {v}')
+    # print('#' * 50 + '\n')
 
     # active_intent_acc = metrics.get_active_intent_accuracy(
     #         frame_ref, frame_hyp)
@@ -577,10 +470,10 @@ def eval_epochs_done_callback(global_vars):
     # goal_accuracy_dict = metrics.get_average_and_joint_goal_accuracy(
     #     frame_ref, frame_hyp, service)
 
-    return metrics
+    return 0 #metrics
 
 
-def get_average_and_joint_goal_accuracy(frame_ref, frame_hyp, service):
+def get_average_and_joint_goal_accuracy(list_acc, slot_active, slot_cat):
     """Get average and joint goal accuracies of a frame.
 
   Args:
@@ -594,10 +487,6 @@ def get_average_and_joint_goal_accuracy(frame_ref, frame_hyp, service):
         all-goal / categorical-goal / non-categorical-goal accuracies.
   """
     goal_acc = {}
-
-    list_acc, slot_active, slot_cat = compare_slot_values(
-        frame_ref["state"]["slot_values"], frame_hyp["state"]["slot_values"], service
-    )
 
     # (4) Average goal accuracy.
     active_acc = [acc for acc, active in zip(list_acc, slot_active) if active]
